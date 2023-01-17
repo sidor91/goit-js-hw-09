433aa20d47c4e1a7037aca7b3d2e9a67afb02041
Serhii Sydorenko <sidorchik91@gmail.com>
2023-01-17 13:30:59 +0200

    task 1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBckgrndColor() {
+    const body = document.querySelector('body');
+    body.style.backgroundColor = getRandomHexColor();
+}
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+
+startButton.addEventListener('click', onStartButtonClick);
+stopButton.addEventListener('click', onStopButtonClick);
+let interval = null;
+
+
+function onStartButtonClick() {
+    interval = setInterval(changeBckgrndColor, 1000);
+    startButton.setAttribute('disabled', true);
+}
+
+function onStopButtonClick() {
+    clearInterval(interval);
+    startButton.removeAttribute('disabled');
+}
